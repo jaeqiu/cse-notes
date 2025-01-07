f66e964fade51fcec3f263ec2a8f5c0b0573cf52
jaeqiu <jaeqiuzhou@gmail.com>
2025-01-07 15:26:30 +0100

    notes

--- jaeqiu_notes_vault/notes_get.bat
+++
@@ -1,8 +0,0 @@
-@echo on
-cd C:\Users\lychee\dev\cse-notes
-color 0a
-git fetch
-git pull
-git status
-timeout /t 2 /nobreak >nul
-exit
--- jaeqiu_notes_vault/notes_save.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-cd C:\Users\lychee\dev\cse-notes
-color 0a
-git status
-git add .
-git commit -m "notes"
-git push
-timeout /t 2 /nobreak >nul
-exit
