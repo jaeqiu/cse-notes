2e6062bfabd2465082236a41277aa917020dc3ea
jaeqiu <jaeqiuzhou@gmail.com>
2025-01-12 16:37:55 +0100

    autosave

--- repo_push.bat
+++ repo_push.bat
@@ -3,15 +3,18 @@ color 0e
 git fetch
 git pull
 timeout /t 1 /nobreak >nul
+cls
 
 color 0b
 git status
 git add .
 timeout /t 1 /nobreak >nul
+cls
 
 color 0a
 git commit -m "autosave"
 git push
 timeout /t 1 /nobreak >nul
+cls
 
 exit
