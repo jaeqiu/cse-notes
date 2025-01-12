3a40ab59139ec189ebb64f8318af74e920ed155d
jaeqiu <jaeqiuzhou@gmail.com>
2025-01-12 17:47:42 +0100

    autosave

--- repo_ignore_apply.bat
+++ repo_ignore_apply.bat
@@ -1,4 +1,14 @@
 @echo on
-color 0f
-git rm -r --cached .  && git add . && timeout /t 2 /nobreak >nul
+color 0e
+git rm -r --cached .  && git status && timeout /t 1 /nobreak >nul
+cls
+
+color 0b
+git add . && timeout /t 1 /nobreak >nul
+cls
+
+color 0a
+git status && timeout /t 3 /nobreak >nul
+cls
+
 exit
