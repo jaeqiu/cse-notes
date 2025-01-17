bd4e93f394c97332b8e417a5672d6ccd889699fd
jaeqiu <jaeqiuzhou@gmail.com>
2025-01-18 00:56:54 +0100

    autosave

--- repo_save.bat
+++ repo_save.bat
@@ -1,28 +1,28 @@
 @echo on
 ::gitignore update
 color 0e
-git rm -r --cached .  && git status :: && timeout /t 1 /nobreak >nul
+git rm -r --cached .  && git status 
 cls
 
 color 0b
-git add . :: && timeout /t 1 /nobreak >nul
+git add . 
 cls
 
 color 0a
-git status :: && timeout /t 1 /nobreak >nul
+git status 
 cls
 
 ::standard push
 color 0e
-git fetch && git pull :: && timeout /t 1 /nobreak >nul
+git fetch && git pull 
 cls
 
 color 0b
-git status && git add . :: && timeout /t 1 /nobreak >nul
+git status && git add . 
 cls
 
 color 0a
-git commit -m "autosave" && git push :: && timeout /t 1 /nobreak >nul
+git commit -m "autosave" && git push 
 cls
 
 exit
